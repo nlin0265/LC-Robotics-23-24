7096d1fd54eed22ae14cc3df990b485a1cde7c12
CreeperNinja265 <nlin102005@gmail.com>
2023-12-01 20:37:21 -0600

    Updated controls/auton 12/1

--- src/main.cpp
+++ src/main.cpp
@@ -27,9 +27,6 @@ void catapult();
 void stopCatapult();
 void lockCatapult();
 void releaseCatapult();
-void armHorizontal(int direction);
-void armForward();
-void armVertical(int direction);
 void flapOpen();
 void flapClose();
 
@@ -40,30 +37,32 @@ void resetPuncher();
 void advance(double moveInches);
 void turn(double driveDegrees);
 void autonSkills();
+void autonSkills2();
 void autonFar();
 void autonClose();
 
 //GLOBAL-SCOPE VARIABLES(Ports)
 #define PORT_LEFT_FRONT 5
 #define PORT_LEFT_MIDDLE 4
-#define PORT_LEFT_BACK 3
+#define PORT_LEFT_BACK_MIDDLE 3
+#define PORT_LEFT_BACK 2
 #define PORT_RIGHT_FRONT 10
 #define PORT_RIGHT_MIDDLE 9
-#define PORT_RIGHT_BACK 8
+#define PORT_RIGHT_BACK_MIDDLE 8
+#define PORT_RIGHT_BACK 7
 #define PORT_CATAPULT 6
-#define PORT_ARM 7
 
 //FILE-SCOPE VARIABLE
 Controller master (E_CONTROLLER_MASTER);
 Motor motorLeftFront   (PORT_LEFT_FRONT,   E_MOTOR_GEARSET_18, 1, E_MOTOR_ENCODER_DEGREES);
 Motor motorLeftMiddle(PORT_LEFT_MIDDLE, E_MOTOR_GEARSET_18, 1, E_MOTOR_ENCODER_DEGREES);
-Motor motorLeftBack  (PORT_LEFT_BACK,  E_MOTOR_GEARSET_18, 0, E_MOTOR_ENCODER_DEGREES);
+Motor motorLeftBackMiddle  (PORT_LEFT_BACK_MIDDLE,  E_MOTOR_GEARSET_18, 0, E_MOTOR_ENCODER_DEGREES);
+Motor motorLeftBack(PORT_LEFT_BACK, E_MOTOR_GEARSET_18, 0, E_MOTOR_ENCODER_DEGREES);
 Motor motorRightFront(PORT_RIGHT_FRONT,   E_MOTOR_GEARSET_18, 0, E_MOTOR_ENCODER_DEGREES);
 Motor motorRightMiddle(PORT_RIGHT_MIDDLE, E_MOTOR_GEARSET_18, 0, E_MOTOR_ENCODER_DEGREES);
-Motor motorRightBack(PORT_RIGHT_BACK, E_MOTOR_GEARSET_18, 1, pros::E_MOTOR_ENCODER_DEGREES);
-
-Motor motorCatapult(PORT_CATAPULT, E_MOTOR_GEARSET_18, 0,E_MOTOR_ENCODER_DEGREES);
-Motor motorArm(PORT_ARM, E_MOTOR_GEARSET_18, 1, E_MOTOR_ENCODER_DEGREES);
+Motor motorRightBackMiddle(PORT_RIGHT_BACK_MIDDLE, E_MOTOR_GEARSET_18, 1, pros::E_MOTOR_ENCODER_DEGREES);
+Motor motorRightBack(PORT_RIGHT_BACK, E_MOTOR_GEARSET_18, 0, E_MOTOR_ENCODER_DEGREES);
+Motor motorCatapult(PORT_CATAPULT, E_MOTOR_GEARSET_18, 1,E_MOTOR_ENCODER_DEGREES);
 
 const double ATOV = 200.0/(128.0/3.0); //analog input to velocity output (equals 4.6875)
 const double ADVANCE = 0.552; //Karstant
@@ -74,7 +73,6 @@ const double PI = M_PI;
 double resetCount = 1;
 double AUTONOMOUS_SPEED = 100.0;
 double motorTemperatureDrive;
-double motorArmTemp;
 bool isLaunch = false;
 bool isFlap = false;
 bool isLock = false;
@@ -108,10 +106,10 @@ void opcontrol() {
   }
 
   //drive loop
-  motorArm.set_zero_position(motorArm.tare_position());
-  armVertical(-1);
-  delay(50);
-  motorArm.move_velocity(0);
+  flapClose();
+  motorCatapult.set_zero_position(0);
+  motorCatapult.move_relative(653, 90);
+  delay(200);
   while (true) {
     if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L1)){/*This button is what allows our robot to break if necessary*/
       brake();
@@ -119,9 +117,7 @@ void opcontrol() {
     else{
       moveUpdate();
       motorTemperatureDrive = motorRightBack.get_temperature();//this allows us to check the motor temperature of the drive in order to help troubleshoot potential problems
-      motorArmTemp = motorArm.get_temperature();
       lcd::set_text(4, to_string(motorTemperatureDrive) + " F");
-      lcd::set_text(3, to_string(motorArmTemp) + " F");
       coast();
     }
 
@@ -150,12 +146,6 @@ void opcontrol() {
     }
     }
     
-    if(master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_X)){
-      armHorizontal(1);
-    }
-    else if(master.get_digital_new_press(E_CONTROLLER_DIGITAL_Y)){
-      armHorizontal(-1);
-    }
 
     delay(10);//the delay allows for all the commands to be executed properly before the loop starts again, which reduces the chances of bugs happening within the code
    }  
@@ -186,15 +176,17 @@ void disabled() {
 //Drive
 void moveUpdate() {
 	//accepts input from controller
-	int forward = master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y); 
-  int rotate = master.get_analog(E_CONTROLLER_ANALOG_RIGHT_X);
+	int moveR = master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y); 
+  int moveL = master.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y);
  
-  motorRightBack.move_velocity((+forward +rotate) * ATOV);
-  motorRightMiddle.move_velocity((+forward +rotate) * ATOV);
-  motorRightFront.move_velocity((+forward +rotate) * ATOV);
-  motorLeftBack.move_velocity((+forward -rotate) * ATOV);
-  motorLeftMiddle.move_velocity((+forward -rotate) * ATOV);
-  motorLeftFront.move_velocity((+forward -rotate) * ATOV);
+  motorRightBack.move_velocity((+moveR)*ATOV);
+  motorRightBackMiddle.move_velocity((+moveR) * ATOV);
+  motorRightMiddle.move_velocity((+moveR) * ATOV);
+  motorRightFront.move_velocity((+moveR) * ATOV);
+  motorLeftBack.move_velocity((+moveL)*ATOV);
+  motorLeftBackMiddle.move_velocity((+moveL) * ATOV);
+  motorLeftMiddle.move_velocity((+moveL) * ATOV);
+  motorLeftFront.move_velocity((+moveL) * ATOV);
 }
 
 //Breaks
@@ -227,7 +219,7 @@ void flapClose(){
 }
 
 void catapult(){
-  motorCatapult.move_velocity(-90);
+  motorCatapult.move_velocity(90);
 }
 
 void stopCatapult(){
@@ -235,16 +227,7 @@ void stopCatapult(){
 }
 
 void lockCatapult(){
-  motorCatapult.move_relative(-660, 50);
-}
-void armHorizontal(int direction){
-  motorArm.move_relative(180*direction, 100);
-}
-void armForward(){
-  motorArm.move_velocity(50);
-}
-void armVertical(int direction){
-  motorArm.move_relative(90*direction, 100);
+  motorCatapult.move_relative(653, 50);
 }
 //Autonmous Period Sub-Functions
 
@@ -256,18 +239,18 @@ void advance(double moveInches) {
 	double motorDegrees = 2*(moveInches) / (WHEEL_DIAMETER * PI) * 360 * ADVANCE;
 
   //robot knows its relative positions
-  motorRightBack.tare_position();
+  motorRightBackMiddle.tare_position();
   motorRightMiddle.tare_position();
   motorRightFront.tare_position();
-  motorLeftBack.tare_position();
+  motorLeftBackMiddle.tare_position();
   motorLeftMiddle.tare_position();
   motorLeftFront.tare_position();
   
   //move robot
-	motorRightBack .move_relative(motorDegrees, AUTONOMOUS_SPEED);
+	motorRightBackMiddle .move_relative(motorDegrees, AUTONOMOUS_SPEED);
 	motorRightMiddle.move_relative(motorDegrees, AUTONOMOUS_SPEED);
 	motorRightFront .move_relative(motorDegrees, AUTONOMOUS_SPEED);
-  motorLeftBack  .move_relative(motorDegrees, AUTONOMOUS_SPEED);
+  motorLeftBackMiddle  .move_relative(motorDegrees, AUTONOMOUS_SPEED);
   motorLeftMiddle.move_relative(motorDegrees, AUTONOMOUS_SPEED);
   motorLeftFront  .move_relative(motorDegrees, AUTONOMOUS_SPEED);
 
@@ -280,8 +263,8 @@ void advance(double moveInches) {
   
   motorRightFront.move_velocity(0);
   motorRightMiddle.move_velocity(0);
-  motorRightBack.move_velocity(0);
-  motorLeftBack.move_velocity(0);
+  motorRightBackMiddle.move_velocity(0);
+  motorLeftBackMiddle.move_velocity(0);
   motorLeftMiddle.move_velocity(0);
   motorLeftFront.move_velocity(0);
 
@@ -299,22 +282,22 @@ void turn(double driveDegrees) {
   //move robot
   motorRightFront .move_relative(-motorDegrees, AUTONOMOUS_SPEED);
   motorRightMiddle.move_relative(-motorDegrees, AUTONOMOUS_SPEED);
-	motorRightBack .move_relative(-motorDegrees, AUTONOMOUS_SPEED);
+	motorRightBackMiddle .move_relative(-motorDegrees, AUTONOMOUS_SPEED);
 	motorLeftFront  .move_relative(+motorDegrees, AUTONOMOUS_SPEED);
   motorLeftMiddle.move_relative(+motorDegrees, AUTONOMOUS_SPEED);
-  motorLeftBack .move_relative(+motorDegrees, AUTONOMOUS_SPEED);
+  motorLeftBackMiddle .move_relative(+motorDegrees, AUTONOMOUS_SPEED);
 
   //elimainates momentum
   while(!(motorLeftFront.get_position() < (motorDegrees + 2) && motorLeftFront.get_position() > (motorDegrees-2))){
     delay(20);
   }
   
-  motorRightBack.move_velocity(0);
+  motorRightBackMiddle.move_velocity(0);
   motorRightMiddle.move_velocity(0);
   motorRightFront.move_velocity(0);
   motorLeftFront.move_velocity(0);
   motorLeftMiddle.move_velocity(0);
-  motorLeftBack.move_velocity(0);
+  motorLeftBackMiddle.move_velocity(0);
 
   coast();
 }
@@ -322,33 +305,46 @@ void turn(double driveDegrees) {
 
 //Auton Period Code
 void autonClose(){
-turn(-45);
-advance(-12);
-turn(90);
-advance(-6);
-armHorizontal(1);
-delay(1000);
-turn(95);
-turn(-15);
-armVertical(-1);
+advance(-8.5);
+flapOpen();
+delay(100);
+advance(3);
 delay(500);
-turn(80);
-advance(-16);
-turn(25);
-advance(-12);
-armForward();
-lcd::set_text(5, "Auton Stop");
+flapClose();
+turn(45);
+advance(12);
+turn(45);
+advance(26);
+flapOpen();
 
 }
 void autonFar(){
-advance(29);
-advance(-28.5);
-turn(-55);
-advance(-20);
-armVertical(1);
-armForward();
+advance(26);
+advance(-24);
+turn(160);
+advance(33);
+flapOpen();
+
+
+
+
+
+
 }
 
 void autonSkills(){
+  catapult();
+}
+
+void autonSkills2(){
+  turn(-45);
+  advance(-12);
+  turn(90);
+  advance(-6);
+  delay(1000);
+  turn(95);
+  turn(-95);
+
+  advance(-7);
   catapult();
 }
